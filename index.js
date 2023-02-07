2e6d15b5e782c36eb6b5b0a3fcca4c4974999f0b
ashrafulasif123 <cseasifiiuc9@gmail.com>
2023-02-07 20:04:36 +0600

    Finall Assignment 4 submit

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 /** 
- * Problem 1: Let’s play a mind game
- * Simple Mathmatical(+, - , *, /) Operator 
+ * Simple Mathmatical(+, - , *, /) Operation 
  */
 function mindGame(number){
     if(typeof number != 'number'){ 
-        return 'Please provide a number'; // if the input is not number
+        return 'Please provide a number'; 
     }
-        return result = ((number * 3) + 10) / 2 -5; //Assigning mathmatical operator to find the result 
+        const result = ((number * 3) + 10) / 2 -5; 
+        return result;
 }
-// console.log(mindGame('4'));
 
 /** 
- * Problem 2: Finding even or odd
- * Total number of characters in String 
+* Total number of characters in String, even or odd 
  */
 function evenOdd(friendName){
     if(typeof friendName == 'string'){
-        const friend = friendName.length;
-        if(friend % 2 == 0){ //checking even or odd
+        let friend = friendName.length;
+        if(friend % 2 == 0){ 
             return 'even';
         }
         return 'odd'
     }
     return 'Please provide a string';
-       
 }
-// console.log(evenOdd('asif'));
 
 /**
- *  Problem 3: Is Less or Greater than seven
  *  The input number is greaterthan or less than from 7 
  */
 function isLGSeven(inputNumber){
@@ -36,20 +31,16 @@ function isLGSeven(inputNumber){
         const theInputNumber = inputNumber;
         const differenceBetween = theInputNumber -7 ;
         if(differenceBetween < 7){
-            return differenceBetween; //if the subtraction  is less then return the subtraction
+            return differenceBetween; 
         }
         else{
-            return inputNumber * 2; //if the subtraction  is greater than 7 then input number(double) return
+            return inputNumber * 2; 
         }
     }
     return 'Please provide a number';
 }
 
-// console.log(isLGSeven(20));
-
-
 /**
- *  Problem 4: Finding Bad data
  *  Finding total Negetive Value 
  */
 function findingBadData(friendsAge){
@@ -57,22 +48,17 @@ function findingBadData(friendsAge){
         const badData = [];
         for(i = 0; i < friendsAge.length; i++){
             const friendAge = friendsAge[i];
-            if (friendAge < 0) {  // checking input array number is positive or negetive value 
-                badData.push(friendAge); // if the age is negetive value then push to the new array 
+            if (friendAge < 0) {  
+                badData.push(friendAge); 
             }
         }
-        const totalBadData = badData.length; // total number of negetive value
+        const totalBadData = badData.length; 
         return totalBadData;
     }
     return 'The input is not a array';
 }
 
-const friendsAge = [50, 100, -87, 33, 78 , -90];
-// // const friendsAge = 'oh my god';
-// console.log(findingBadData(friendsAge));
-
 /**
- *  Problem 5: Convert your gems into diamond
  *  gems to diamond Converter 
  */
     function gemsToDiamond(firstFriendGemsQuantity, secondFriendGemsQuantity, thirdFriendGemsQuantity ){
@@ -81,19 +67,19 @@ const friendsAge = [50, 100, -87, 33, 78 , -90];
             const secondFriendGemsPower = 32;
             const thirdFriendGemsPower = 43;
 
-            const firstFriendGem = firstFriendGemsPower * firstFriendGemsQuantity;
-            const secondFriendGem = secondFriendGemsPower * secondFriendGemsQuantity;
-            const thirdFriendGem = thirdFriendGemsPower * thirdFriendGemsQuantity;
-            const totalDiamond = firstFriendGem + secondFriendGem + thirdFriendGem; // summation of total diamond
-            if(totalDiamond < 1000*2){ // checking the total diamond is less or greater than 2000
-                return totalDiamond; // return the total diamond is less than 2000
+            let firstFriendGem = firstFriendGemsPower * firstFriendGemsQuantity;
+            let secondFriendGem = secondFriendGemsPower * secondFriendGemsQuantity;
+            let thirdFriendGem = thirdFriendGemsPower * thirdFriendGemsQuantity;
+            let totalDiamond = firstFriendGem + secondFriendGem + thirdFriendGem; 
+            if(totalDiamond < 1000*2){ 
+                return totalDiamond; 
             }
-            return totalDiamond - 2000; // return the total diamond is greater than 2000
+            return totalDiamond - 2000;
         }
         return 'please enter all input in number ';  
        
     }
-    // console.log(gemsToDiamond(50, 47, 50));
+    
 
 
 
